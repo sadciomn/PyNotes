293e620b16fb57db257b99d96d9a7de609189484
sadciomn <sadciomngit@gmail.com>
2024-09-26 20:07:06 +0300

    Added functions file

---
+++ functions.py
@@ -0,0 +1,6 @@
+class Functions:
+    def create_file(self):
+        pass
+
+    def save_file(self):
+        pass
--- main.py
+++ main.py
@@ -1,7 +1,2 @@
 def main():
-    text = input()
-    test = input()
-
-
-if __name__ == "__main__":
-    main()
+    pass
